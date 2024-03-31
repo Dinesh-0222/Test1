a6cdc9c5935ba043437ecac166ed90454628155d
Dinesh <02220232.cst@rub.edu.bt>
2024-03-31 22:33:41 +0530

    first

--- lab_3.py
+++ lab_3.py
@@ -3,7 +3,7 @@ if age<=12:
          print("You are eligible for discount on the movie ticket")
 elif age >=13 and age<=18:
           student =input("Are you student:")
-          if student=='yes' or student== 'Yes':
+          if student=='yes' or student== 'Yes' or student == 'Y' or student == 'y':
                     print("You are eligible for discount on the movie ticket")
           else:
                     print("You are not eligible for the discount on the movie ticket")
